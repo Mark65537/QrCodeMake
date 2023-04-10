febd91a369b14070fc60232b57b5b3e3db022ffd
User <User@DESKTOP-N5SNVDJ>
2023-04-10 17:55:48 +0700

    создание класса MailMessageInfo

---
+++ QrCodeMake-WinForm/Classes/MailMessageInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QrCodeMake_WinForm.Classes
+{
+    internal class MailMessageInfo
+    {
+        public string emailFrom;
+        public string emailTo;
+        public SecureString pass;
+        public string body;
+        public string confDic;
+        public string subject;
+        public string provider;
+    }
+}
